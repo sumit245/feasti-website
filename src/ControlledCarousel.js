0b62760192f1d6f8918528e42bc1dbd02d7ce480
sumit245 <sumitranjan245@gmail.com>
2023-02-04 00:43:20 +0530

    Message

--- src/ControlledCarousel.js
+++ src/ControlledCarousel.js
@@ -8,42 +8,71 @@ const androidUrl = 'https://play.google.com/store/apps';
 
 export default function BannerCarousel() {
   return (
-    <div className='hero-container'>
-      <div className="slider">
-        <div className="slides">
-          <div id='slides__1' className='slide'>
-            <div>
-              <h1 className="slides__text">Subscription based homemade<br /> meal plan</h1>
-              <span className="slides__text">Order your meal from our app</span>
-              <div className="d-inline-flex align-items-start justify-content-center">
-                <div style={{ height: 40, width: 160, padding: 0 }}>
-                  <a
-                    href="https://apps.apple.com/us/app/feasti-homecooked-food/id1645705596"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    className='app-store-link'
-                  >
-                  </a>
-                </div>
-                <div style={{ height: 40, width: 160, padding: 0, marginTop: -10 }}>
-                  <a
-                    href="https://play.google.com/store/apps"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    title="Coming Soon"
-                    className='play-store-link'>
-                  </a>
-                </div>
+    <div className="slider">
+      <div className="slides">
+        <div id='slides__1' className='slide'>
+          <div>
+            <h1 className="slide__text text-white">Subscription based homemade meal plan</h1>
+            <span className="text-white">Order your meal from our app</span>
+            <div className="d-inline-flex align-items-start justify-content-center my-2">
+              <div style={{ height: 32, width: 128, padding: 0 }}>
+                <a
+                  href="https://apps.apple.com/us/app/feasti-homecooked-food/id1645705596"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className='app-store-link'
+                >
+                </a>
               </div>
-              <img
-                src={require('./assets/home_screen_user.png')}
-                className="hero-img"
-                height={600}
-                width={240}
-              />
+              <div style={{ height: 32,  width: 128, padding: 0, marginTop:-10 }}>
+                <a
+                  href="https://play.google.com/store/apps"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  title="Coming Soon"
+                  className='play-store-link'>
+                </a>
+              </div>
             </div>
+            <img
+              src={require('./assets/user-home-final.png')}
+              className="hero-img"
+              height={360}
+              width="auto"
+            />
           </div>
-
+        </div>
+        <div id='slides__1' className='slide'>
+          <div>
+            <h1 className="slides__text">Subscription based homemade<br /> meal plan</h1>
+            <span className="slides__text">Order your meal from our app</span>
+            <div className="d-inline-flex align-items-start justify-content-center">
+              <div style={{ height: 40, width: 160, padding: 0 }}>
+                <a
+                  href="https://apps.apple.com/us/app/feasti-homecooked-food/id1645705596"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className='app-store-link'
+                >
+                </a>
+              </div>
+              <div style={{ height: 40, width: 160, padding: 0, marginTop: -10 }}>
+                <a
+                  href="https://play.google.com/store/apps"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  title="Coming Soon"
+                  className='play-store-link'>
+                </a>
+              </div>
+            </div>
+            <img
+              src={require('./assets/user-home-final.png')}
+              className="hero-img"
+              height={600}
+              width={240}
+            />
+          </div>
         </div>
       </div>
     </div>
